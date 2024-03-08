bf520883f84c22e7b8b1ab81adee3d6e6226b7aa
Giang <giang.truong@fidt.vn>
2024-03-08 11:13:56 +0700

    change the place to save data

--- covid19/assets.py
+++ covid19/assets.py
@@ -1,6 +1,61 @@
 import pandas as pd
+import sqlalchemy as sa
 import os
+
+from urllib.parse import quote_plus
 from dagster import asset
+
+DRIVER = "ODBC Driver 17 for SQL Server"
+SERVER_NAME = "VHLC-GIANGTD13"
+DATABASE_NAME = "cid"
+USER_NAME = "cid-local"
+USER_PASSWORD = "654321"
+
+connection_string = (
+    f'Driver={DRIVER};'
+    f'SERVER={SERVER_NAME};'
+    f'Database={DATABASE_NAME};'
+    f'UID={USER_NAME};'
+    f'PWD={USER_PASSWORD};'
+    'Trusted_Connection=no;'
+)
+
+connection_uri = f"mssql+pyodbc:///?odbc_connect={quote_plus(connection_string)}"
+engine = sa.create_engine(connection_uri, fast_executemany=True)
+
+def read_table_from_db(
+        query_string:str
+    ):
+    """
+    Read data from the database, and assign the output as a dataframe.
+    """
+    df = pd.read_sql( 
+        sql=query_string, 
+        con=engine
+    )
+
+    return df
+
+def write_data_to_db(
+        df:pd.DataFrame, 
+        table_name:str, 
+        schema:str, 
+        insert_type="replace", 
+        chunks=None
+    ):
+    """
+    Write data from a dataframe to the database.
+    """
+    df.to_sql(
+        name=table_name, 
+        schema=schema, 
+        con=engine, 
+        if_exists=insert_type, 
+        chunksize=chunks, 
+        index=False
+
+    )
+    return df
 
 @asset
 def pull_cases() -> None:
@@ -15,46 +70,37 @@ def pull_cases() -> None:
     # rename the country column name
     df.rename(columns={"location":"country"}, inplace=True)
 
-    # make sure that model folder is created
-    os.makedirs("model", exist_ok=True)
-
-    # write data to database or elsewhere
-    df.to_parquet("model/f_daily_cases.parquet.gz", index=False)
+    # write data to database
+    write_data_to_db(df=df, table_name="daily_cases", schema="fact")
 
 @asset(deps=[pull_cases])
 def generate_calendar() -> None:
     """
-    Create a date calendar based on the f_daily_cases.
+    Create a date calendar based on the daily_cases.
     """
 
-    # read the fact table
-    df = pd.read_parquet("model/f_daily_cases.parquet.gz")
-
     # get the unique dates
-    df_date = df[["date"]].astype("datetime64[ns]").drop_duplicates()
+    df = read_table_from_db("select distinct [date] from fact.daily_cases")
 
     # add more attributes
-    df_date["year"] = df["date"].dt.year
-    df_date["month"] = df["date"].dt.month
-    df_date["day"] = df["date"].dt.day
+    df["year"] = df["date"].dt.year
+    df["month"] = df["date"].dt.month
+    df["day"] = df["date"].dt.day
 
-    # write data to database or elsewhere
-    df_date.to_parquet("model/d_date.parquet.gz", index=False)
+    # write data to database
+    write_data_to_db(df=df, table_name="calendar", schema="dim")
 
 @asset(deps=[pull_cases])
 def generate_countries() -> None:
     """
-    Create a country table based on the f_daily_cases.
+    Create a country table based on the daily_cases.
     """
 
-    # make sure that model folder is created
-    df = pd.read_parquet("model/f_daily_cases.parquet.gz")
-
     # get the unique countries
-    df_country = df[["country"]].drop_duplicates()
+    df = read_table_from_db("select distinct [country] from fact.daily_cases")
 
-    # write data to database or elsewhere
-    df_country.to_parquet("model/d_country.parquet.gz", index=False)
+    # write data to database
+    write_data_to_db(df=df, table_name="country", schema="dim")
 
 @asset(deps=[generate_calendar, generate_countries])
 def pull_deaths() -> None:
@@ -73,18 +119,18 @@ def pull_deaths() -> None:
     df.rename(columns={"location":"country"}, inplace=True)
 
     # read dimension tables
-    df_date = pd.read_parquet("model/d_date.parquet.gz")
-    df_country = pd.read_parquet("model/d_country.parquet.gz")
+    df_date = read_table_from_db("select distinct [date] from dim.calendar")
+    df_country = read_table_from_db("select distinct [country] from dim.country")
 
     # inner join to make sure foreign keys
     df = (
         df
-        .merge(df_date[["date"]])
-        .merge(df_country[["country"]])
+        .merge(df_date)
+        .merge(df_country)
     )
 
-    # write data to database or elsewhere
-    df.to_parquet("model/f_daily_deaths.parquet.gz", index=False)
+    # write data to database
+    write_data_to_db(df=df, table_name="daily_deaths", schema="fact")
 
 @asset(deps=[generate_calendar, generate_countries])
 def pull_vaccinations() -> None:
@@ -100,18 +146,18 @@ def pull_vaccinations() -> None:
     df.rename(columns={"location":"country"}, inplace=True)
 
     # read dimension tables
-    df_date = pd.read_parquet("model/d_date.parquet.gz")
-    df_country = pd.read_parquet("model/d_country.parquet.gz")
+    df_date = read_table_from_db("select distinct [date] from dim.calendar")
+    df_country = read_table_from_db("select distinct [country] from dim.country")
 
     # inner join to make sure foreign keys
     df = (
         df
-        .merge(df_date[["date"]])
-        .merge(df_country[["country"]])
+        .merge(df_date)
+        .merge(df_country)
     )
 
-    # write data to database or elsewhere
-    df.to_parquet("model/f_daily_vaccinations.parquet.gz", index=False)
+    # write data to database
+    write_data_to_db(df=df, table_name="daily_vaccinations", schema="fact")
 
 @asset(deps=[generate_calendar, generate_countries])
 def pull_hospital_admissions() -> None:
@@ -127,18 +173,18 @@ def pull_hospital_admissions() -> None:
     df.rename(columns={"location":"country"}, inplace=True)
 
     # read dimension tables
-    df_date = pd.read_parquet("model/d_date.parquet.gz")
-    df_country = pd.read_parquet("model/d_country.parquet.gz")
+    df_date = read_table_from_db("select distinct [date] from dim.calendar")
+    df_country = read_table_from_db("select distinct [country] from dim.country")
 
     # inner join to make sure foreign keys
     df = (
         df
-        .merge(df_date[["date"]])
-        .merge(df_country[["country"]])
+        .merge(df_date)
+        .merge(df_country)
     )
 
-    # write data to database or elsewhere
-    df.to_parquet("model/f_daily_hospital_admissions.parquet.gz", index=False)
+    # write data to database
+    write_data_to_db(df=df, table_name="daily_hospital_admissions", schema="fact")
 
 @asset(deps=[generate_calendar, generate_countries])
 def pull_excess_mortality() -> None:
@@ -154,15 +200,15 @@ def pull_excess_mortality() -> None:
     df.rename(columns={"location":"country"}, inplace=True)
 
     # read dimension tables
-    df_date = pd.read_parquet("model/d_date.parquet.gz")
-    df_country = pd.read_parquet("model/d_country.parquet.gz")
+    df_date = read_table_from_db("select distinct [date] from dim.calendar")
+    df_country = read_table_from_db("select distinct [country] from dim.country")
 
     # inner join to make sure foreign keys
     df = (
         df
-        .merge(df_date[["date"]])
-        .merge(df_country[["country"]])
+        .merge(df_date)
+        .merge(df_country)
     )
 
-    # write data to database or elsewhere
-    df.to_parquet("model/f_daily_excess_mortality.parquet.gz", index=False)
+    # write data to database
+    write_data_to_db(df=df, table_name="daily_excess_mortality", schema="fact")
